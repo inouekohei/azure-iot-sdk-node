171373cc1a86965c0a1c44af874659ea6b03f097
Zoltan Varga <zolvarga@microsoft.com>
2020-09-01 13:31:09 -0700

    chore(azure-iot-digitaltwins-service): fix samples (#881)
    * chore(azure-iot-digitaltwins-service): fix samples
    
    * chore(azure-iot-digitaltwins-service): change name
    
    * chore(azure-iot-digitaltwins-service): change name
    
    * chore(azure-iot-digitaltwins-service): add sample
    
    * fix(azure-iot-digitaltwins-service): fix lint errs

--- digitaltwins/samples/service/typescript/get_digital_twins.ts
+++ digitaltwins/samples/service/typescript/get_digital_twins.ts
@@ -1,7 +1,8 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import * as dt from 'azure-iot-digitaltwins-service';
+import { DigitalTwinServiceClient } from 'azure-iot-digitaltwins-service';
+import { IoTHubTokenCredentials } from 'azure-iot-digitaltwins-service';
 
 // Simple example of how to:
 // - create a Digital Twin Service Client using the DigitalTwinServiceClient constructor
@@ -11,13 +12,22 @@ import * as dt from 'azure-iot-digitaltwins-service';
 // - Environment variables have to be set
 // - Twin enabled device must exist on the ADT hub
 const deviceId = process.env.IOTHUB_DEVICE_ID || '';
-const connString = process.env.IOTHUB_CONNECTION_STRING || '';
+const connectionString = process.env.IOTHUB_CONNECTION_STRING || '';
 const metadata = '$metadata';
 const model = '$model';
 
-const dtServiceClient = new dt.DigitalTwinServiceClient(new dt.IoTHubTokenCredentials(connString));
+async function asyncMain(): Promise<void> {
+  const digitalTwinServiceClient = new DigitalTwinServiceClient(new IoTHubTokenCredentials(connectionString));
+  const deviceTwin = await digitalTwinServiceClient.getDigitalTwin(deviceId);
 
-const twin = dtServiceClient.getDigitalTwin(deviceId);
-if (!!(twin)) {
-  console.log(twin[metadata][model]);
+  // Print the response
+  if (!!(deviceTwin)) {
+    console.log(deviceTwin[metadata][model]);
+  }
 }
+
+asyncMain().catch((err) => {
+  console.log('error code: ', err.code);
+  console.log('error message: ', err.message);
+  console.log('error stack: ', err.stack);
+});
